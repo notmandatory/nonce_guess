bb8335e1ebeb14c14e580d091c4a186b5881c586
bnonni <me@bryanwnonni.com>
2023-02-21 16:53:06 -0500

    change port for server

--- ng_server/src/main.rs
+++ ng_server/src/main.rs
@@ -71,7 +71,7 @@ async fn main() {
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8081));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8082));
     info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
