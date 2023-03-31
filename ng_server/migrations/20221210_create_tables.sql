45d4a9d53baeed36967af8744092801fa93e8ffc
bnonni <bryan@nonni.io>
2023-03-31 18:13:26 -0400

    fix breaking change: remove the UNIQUE contraint from player_name and block in guess table

--- ng_server/migrations/20221210_create_tables.sql
+++ ng_server/migrations/20221210_create_tables.sql
@@ -1,2 +1,2 @@
 CREATE TABLE target (block INTEGER, nonce INTEGER WITHOUT ROWID, UNIQUE(block));
-CREATE TABLE guess (player_name TEXT NOT NULL, block INTEGER REFERENCES target (block), nonce INTEGER NOT NULL, UNIQUE(player_name, block));
+CREATE TABLE guess (player_name TEXT NOT NULL, block INTEGER REFERENCES target (block), nonce INTEGER NOT NULL);
