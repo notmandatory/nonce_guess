37d4410ace52e417572654f1737bd39fbcd76548
Bryan <bryan@nonni.io>
2023-04-05 10:25:18 -0400

    Update ng_server/migrations/20221210_create_tables.sql
    Co-authored-by: Steve Myers <github@notmandatory.org>

--- ng_server/migrations/20221210_create_tables.sql
+++ ng_server/migrations/20221210_create_tables.sql
@@ -1,2 +1,3 @@
 CREATE TABLE target (block INTEGER, nonce INTEGER WITHOUT ROWID, UNIQUE(block));
-CREATE TABLE guess (player_name TEXT NOT NULL, block INTEGER REFERENCES target (block), nonce INTEGER NOT NULL);
+CREATE TABLE guess (player_name TEXT NOT NULL, block INTEGER REFERENCES target (block), nonce INTEGER NOT NULL, UNIQUE(player_name, block));
+CREATE UNIQUE INDEX idx_guess_null_block ON guess(player_name) WHERE block IS NULL;
