fbf8c1f917e6319bb2f36f11b593b8b499c0cac9
Steve Myers <steve@notmandatory.org>
2023-05-02 11:59:21 -0500

    Add unique constraint for guessed nonce

--- ng_server/migrations/20221210_create_tables.sql
+++ ng_server/migrations/20221210_create_tables.sql
@@ -1,3 +1,4 @@
 CREATE TABLE target (block INTEGER, nonce INTEGER WITHOUT ROWID, UNIQUE(block));
-CREATE TABLE guess (player_name TEXT NOT NULL, block INTEGER REFERENCES target (block), nonce INTEGER NOT NULL, UNIQUE(player_name, block));
-CREATE UNIQUE INDEX idx_guess_null_block ON guess(player_name) WHERE block IS NULL;
+CREATE TABLE guess (player_name TEXT NOT NULL, block INTEGER REFERENCES target (block), nonce INTEGER NOT NULL, UNIQUE(player_name, block), UNIQUE(nonce, block));
+CREATE UNIQUE INDEX idx_guess_player_name_null_block ON guess(player_name) WHERE block IS NULL;
+CREATE UNIQUE INDEX idx_guess_nonce_null_block ON guess(nonce) WHERE block IS NULL;
